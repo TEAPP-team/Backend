4e65044c022d13841dc6301ecfa3b3aacea00937
Mikhail Komarov <the237th@yandex.com>
2020-11-18 19:43:31 +0300

    Fixed some bugs

--- src/main/kotlin/com/teapp/Config.kt
+++ src/main/kotlin/com/teapp/Config.kt
@@ -1,12 +1,17 @@
 package com.teapp
 
-import com.zaxxer.hikari.*
+import com.zaxxer.hikari.HikariDataSource
 
 internal class Config {
     companion object {
         val dataSource: HikariDataSource
             get() {
-
+                val ds = HikariDataSource()
+                ds.jdbcUrl = "jdbc:mysql://"
+                ds.username = ""
+                ds.password = ""
+                ds.maximumPoolSize = 1
+                return ds
             }
     }
 }
