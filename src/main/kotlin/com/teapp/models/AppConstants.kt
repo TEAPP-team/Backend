3885b345c1ac217c1abe99dde992d04afd6acaa8
JustContestIt <nikita.beryoza@gmail.com>
2020-12-08 13:53:32 +0300

    Add AppConstants

--- src/main/kotlin/com/teapp/models/AppConstants.kt
+++ src/main/kotlin/com/teapp/models/AppConstants.kt
@@ -1,5 +1,15 @@
 package main.kotlin.com.teapp.models
 
-enum class AppConstants(val value:String) {
-    COOKIE_NAME("TEAPP_COOKIE")
+enum class AppStrings(val value:String) {
+    COOKIE_NAME("TEAPP_COOKIE"),
+    ID_NAME("id"),
+    LOGIN_NAME("login"),
+    PASSWORD_NAME("password"),
+    EMPTY_STRING("");
+}
+
+enum class AppInts(val value: Int) {
+    ID_INDEX(0),
+    LOGIN_INDEX(1),
+    PASSWORD_INDEX(2);
 }
