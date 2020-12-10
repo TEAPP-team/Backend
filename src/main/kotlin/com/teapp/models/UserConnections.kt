1d3c8cee7dd5fd3e2109a1e29477e1792bc6b968
JustContestIt <nikita.beryoza@gmail.com>
2020-12-11 02:58:15 +0300

    Realized data class UserConnections

--- src/main/kotlin/com/teapp/models/UserConnections.kt
+++ src/main/kotlin/com/teapp/models/UserConnections.kt
@@ -1,14 +1,28 @@
 package com.teapp.models
 
+import com.google.gson.GsonBuilder
 import java.time.LocalDate
+import java.util.*
 
 data class UserConnections(val id: Int) {
-    var userSession: String = "username"
-    var userId: Int? = null
+    var personId: Int? = null
+    var session = SessionInfo()
 
     class SessionInfo {
         var accessToken: String? = null
-        var expiredDate: LocalDate? = null
-        var isLoggedOut = true
+        var expiredDate: String? = null
+        var isLoggedOut: Boolean? = null
+    }
+
+    constructor(id: Int, user: User): this(id) {
+        personId = user.id
+        this.session.accessToken = UUID.randomUUID().toString()
+        this.session.expiredDate = LocalDate.now().plusDays(1.toLong()).toString()
+        this.session.isLoggedOut = false
+    }
+
+    fun toJson(): String {
+        val gson = GsonBuilder().serializeNulls().disableHtmlEscaping().setPrettyPrinting().create()
+        return gson.toJson(this)
     }
 }
