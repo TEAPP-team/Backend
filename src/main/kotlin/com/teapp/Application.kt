ff80d318bc1a2ae11441d469a311a0871a62a228
pappl-b <papplb@github.com>
2020-12-17 23:53:28 +0300

    SoMeThInG to be saved

--- src/main/kotlin/com/teapp/Application.kt
+++ src/main/kotlin/com/teapp/Application.kt
@@ -7,6 +7,11 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.features.ContentNegotiation
 import io.ktor.gson.*
+import io.ktor.html.*
+import io.ktor.html.HtmlContent
+import io.ktor.request.*
+import kotlinx.html.*
+import kotlinx.html.stream.createHTML
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -17,17 +22,45 @@ fun Application.module(testing: Boolean = false) {
     install(Routing) {
         route("/api") {
             get("/teahouses/{id}") {
-                try{
+                try {
                     val id = call.parameters["id"]!!.toInt()
                     val teahouse = Teahouse(id)
-                    if(teahouse.fetchDataFromDB(dataFactory)) {
+                    if (teahouse.fetchDataFromDB(dataFactory)) {
                         call.respond(teahouse)
                     }
+                } catch (invalidIdException: NumberFormatException) {
                 }
-                catch(invalidIdException: NumberFormatException) {}
+            }
+            route("/admin") {
+                get() {
+                    call.respondHtml {
+                        head {
+                            title { "ADMIN" }
+                        }
+                        body {
+                            form(action = "/api/admin/login") {
+                                div { "user:" }
+                                div { input(type = InputType.text, name = "username") }
+                                div { "password:" }
+                                div { input(type = InputType.password, name = "password") }
+                                div { input(type = InputType.submit) }
+                            }
+                        }
+                    }
+                }
+
+            }
+            get("/admin/login") {
+                val post = call.receiveParameters()
+                if (post["username"] == "idonotknow" && post["password"] == "whymorgenshtern") {
+                    call.respondText("You are welcome")
+                } else {
+                    call.respondText("You are not welcome")
+                }
             }
         }
     }
+
     install(ContentNegotiation) {
         gson{
             setPrettyPrinting()
